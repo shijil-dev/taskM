8c8aee577b451faa6e3ffce69e13a6489d539b72
dave <shijildave@gmail.com>
2023-06-12 18:13:21 +0530

    changed mongodb uri

--- api/server.js
+++ api/server.js
@@ -2,16 +2,27 @@ const express = require("express")
 const mongoose = require("mongoose")
 const dotenv = require("dotenv").config()
 const app = express()
-
+const Task = require("./model/taskModel.js")
 const PORT = "3000"
+
+//middleware
+/*const logger = (req, res, next) => {
+  console.log("middleware ran")
+  next()
+}*/
+app.use(express.json())
 
 app.get("/", (req, res)=> {
 res.send("home page")
 })
 
 app.post("/api/tasks", async (req, res) => {
-  console.log(req.body)
-  res.send("Task created")
+  try{
+    const task = await Task.create(req.body)
+    res.status(200).json(task)
+  } catch(error){
+    res.status(500).json({msg: error.message})
+  }
 })
 
 mongoose
