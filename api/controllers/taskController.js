d03f33b46c22c382c597529172ddcbe24636f774
dave <shijildave@gmail.com>
2023-06-14 21:47:53 +0530

    fetch a task with id params

--- api/controllers/taskController.js
+++ api/controllers/taskController.js
@@ -14,7 +14,22 @@ const createTask = async (req, res) => {
 const getTasks = async (req, res) => {
   {
   try{
-    const task = await Task.find(req.body)
+    const tasks = await Task.find(req.body)
+    res.status(200).json(tasks)
+  } catch(error){
+    res.status(500).json({msg: error.message})
+  }
+  }
+}
+//get a single task with id params
+const getTask = async (req, res) => {
+  {
+  try{
+    const {id} = req.params
+    const task = await Task.findById(id)
+    if ( !task ){
+        return res.status(404).json(`no task with id ${id}`)
+      }
     res.status(200).json(task)
   } catch(error){
     res.status(500).json({msg: error.message})
@@ -22,8 +37,8 @@ const getTasks = async (req, res) => {
   }
 }
 
-
 module.exports = {
   createTask,
-  getTasks
+  getTasks,
+  getTask
 }
--- api/routes/taskRoutes.js
+++ api/routes/taskRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express")
-const { getTasks , createTask } = require("../controllers/taskController.js")
+const { getTasks , createTask ,getTask } = require("../controllers/taskController.js")
 const routes = express.Router()
 
 routes.post("/api/tasks", createTask)
 
 routes.get("/api/tasks", getTasks)
 
+routes.get("/api/tasks/:id",getTask)
 
 module.exports = routes
